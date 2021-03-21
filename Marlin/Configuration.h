4431df8a54ae3228f81f15f95dbe316859a35f36
xantrk-monster <furkancetin@yahoo.com>
2021-03-21 22:11:24 +0300

    fix ubl leveling temps from  jyers

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1331,7 +1331,7 @@
 /**
  * Auto-leveling needs preheating
  */
-//#define PREHEAT_BEFORE_LEVELING
+#define PREHEAT_BEFORE_LEVELING
 #if ENABLED(PREHEAT_BEFORE_LEVELING)
   #define LEVELING_NOZZLE_TEMP 210   // (°C) Only applies to E0 at this time
   #define LEVELING_BED_TEMP     70
