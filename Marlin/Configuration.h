b34cada9e168f70ef2fa8f118e952229c10dc6e4
xantrk-monster <furkancetin@yahoo.com>
2021-05-08 23:37:18 +0300

    pid hotend

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -519,9 +519,9 @@
     #define DEFAULT_Kd_LIST {  78.81,  78.81 }
   #else
     // Ender 3 v2
-    #define DEFAULT_Kp  28.72
-    #define DEFAULT_Ki   2.62
-    #define DEFAULT_Kd  78.81
+    #define DEFAULT_Kp  17.65
+    #define DEFAULT_Ki   1.12
+    #define DEFAULT_Kd  68.99
   #endif
 #endif // PIDTEMP
 
